69122939b1dc4146550173a5d469760591c69e1d
HTTAM <tamht298@gmail.com>
2020-11-03 15:55:48 +0700

    change to be static method in factory

--- src/vn/com/tma/FactoryPattern/FactoryMain.java
+++ src/vn/com/tma/FactoryPattern/FactoryMain.java
@@ -3,9 +3,9 @@ package vn.com.tma.FactoryPattern;
 public class FactoryMain {
     public static void main(String[] args) {
         Customer cus1 = new Customer("Tam");
-        cus1.callPizza(new PizzaFactory().getPizza(PizzaType.CHEESE), 1);
+        cus1.callPizza(PizzaFactory.getPizza(PizzaType.CHEESE), 1);
 
         Customer cus2 = new Customer("Tam2");
-        cus2.callPizza(new PizzaFactory().getPizza(PizzaType.VEGGIE), 2);
+        cus2.callPizza(PizzaFactory.getPizza(PizzaType.VEGGIE), 2);
     }
 }
--- src/vn/com/tma/FactoryPattern/PizzaFactory.java
+++ src/vn/com/tma/FactoryPattern/PizzaFactory.java
@@ -2,7 +2,7 @@ package vn.com.tma.FactoryPattern;
 
 public class PizzaFactory {
 
-    public Pizza getPizza(PizzaType pizzaType) {
+    public static Pizza getPizza(PizzaType pizzaType) {
         if (pizzaType.equals(null)) return null;
 
         Pizza pizza = null;
@@ -13,7 +13,6 @@ public class PizzaFactory {
             case VEGGIE: {
                 pizza = new VeggiePizza();
             }
-
         }
         return pizza;
     }
