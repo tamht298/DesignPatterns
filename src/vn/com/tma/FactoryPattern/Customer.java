f7532a6b4e6a4cbabe7768cb9915b5ed2c064804
HTTAM <tamht298@gmail.com>
2020-11-03 15:34:52 +0700

    remove Pizza obj in Customer

--- src/vn/com/tma/FactoryPattern/Customer.java
+++ src/vn/com/tma/FactoryPattern/Customer.java
@@ -3,8 +3,6 @@ package vn.com.tma.FactoryPattern;
 public class Customer {
     private String name;
 
-    private Pizza pizza;
-
     public String getName() {
         return name;
     }
@@ -19,7 +17,6 @@ public class Customer {
     }
 
     public void callPizza(Pizza pizza, int quantity) {
-        this.pizza = pizza;
-        System.out.println(quantity + " pieces of " + this.pizza.getPizzaTypeName() + " for " + this.name);
+        System.out.println(quantity + " pieces of " + pizza.getPizzaTypeName() + " for " + this.name);
     }
 }
